304ccff91292faf66b95ded3bc1f7e1f89032dc6
Corey Leath <corey22blue@hotmail.com>
2026-04-03 13:35:10 -0400

    Add Prometheus request counter
    Added a Prometheus counter to track total requests.

--- main.go
+++ main.go
@@ -1,58 +1,11 @@
-package main
-
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type Request struct {
-	Features []float64 `json:"features"`
-}
-
-func main() {
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Go Prediction API Running"})
-	})
-
-	r.POST("/predict", func(c *gin.Context) {
-		var req Request
-
-		if err := c.BindJSON(&req); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-
-		payload, _ := json.Marshal(req)
-
-		resp, err := http.Post(
-			"http://localhost:8001/predict",
-			"application/json",
-			bytes.NewBuffer(payload),
-		)
-
-		if err != nil {
-			c.JSON(500, gin.H{"error": "ML service unavailable"})
-			return
-		}
-
-		defer resp.Body.Close()
-
-		body, _ := ioutil.ReadAll(resp.Body)
-
-		var result map[string]interface{}
-		json.Unmarshal(body, &result)
-
-		c.JSON(200, gin.H{
-			"prediction": result["prediction"],
-		})
-	})
-
-	r.Run(":8080")
-}
+var requestCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "requests_total",
+		Help: "Total number of requests",
+	},
+)
