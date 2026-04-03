49f7bbba48dd7ff1e3b05ad9f1d4adef932bf2ff
Corey Leath <corey22blue@hotmail.com>
2026-04-03 13:32:57 -0400

    Load model from MLflow instead of defining SimpleModel
    Replaced SimpleModel class with loading a model from MLflow.

--- model.py
+++ model.py
@@ -1,15 +1,8 @@
+import mlflow.pytorch
 import torch
-import torch.nn as nn
 
-class SimpleModel(nn.Module):
-    def __init__(self):
-        super(SimpleModel, self).__init__()
-        self.fc = nn.Linear(4, 1)
-
-    def forward(self, x):
-        return torch.sigmoid(self.fc(x))
-
-model = SimpleModel()
+# Load latest model
+model = mlflow.pytorch.load_model("runs:/<RUN_ID>/model")
 
 def predict(input_data):
     with torch.no_grad():
